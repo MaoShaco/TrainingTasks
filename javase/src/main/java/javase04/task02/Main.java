c58cff5339cc252188c536a1a3e5a542637c7c53
Mao Shaco <maoshaco@gmail.com>
2015-10-31 11:28:06 +0300

    task02 added

--- javase/src/main/java/javase04/task02/Main.java
+++ javase/src/main/java/javase04/task02/Main.java
@@ -1,10 +1,24 @@
 package javase04.task02;
 
+import javase04.task01.util.JavaKeywordDictionary;
+import org.apache.commons.io.IOUtils;
+
+import java.io.*;
+
 /**
  * Created by Mao Shaco on 10/31/2015.
  */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello task 02");
+    public static void main(String[] args) throws IOException {
+
+        Writer out = new FileWriter("KeyWordsCountSymbolLike.txt");
+        Reader in = new FileReader("Main.java");
+
+        try {
+            IOUtils.write(JavaKeywordDictionary.getKeywordCount(IOUtils.toString(in)).toString(), out);
+        } finally {
+            IOUtils.closeQuietly(in);
+            IOUtils.closeQuietly(out);
+        }
     }
 }
