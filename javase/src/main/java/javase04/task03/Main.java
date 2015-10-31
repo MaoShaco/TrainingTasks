8b6ad1a110e50357ecd47546e9805636f1bba835
Mao Shaco <maoshaco@gmail.com>
2015-10-31 11:28:15 +0300

    task03 added

--- javase/src/main/java/javase04/task03/Main.java
+++ javase/src/main/java/javase04/task03/Main.java
@@ -1,10 +1,26 @@
 package javase04.task03;
 
+import org.apache.commons.io.IOUtils;
+
+import java.io.*;
+import java.nio.charset.Charset;
+
 /**
  * Created by Mao Shaco on 10/31/2015.
  */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello task 03");
+    public static void main(String[] args) throws IOException {
+
+        OutputStream ous = new FileOutputStream("UTF8-UTF16.txt");
+        OutputStreamWriter out = new OutputStreamWriter(ous, Charset.forName("UTF-16"));
+
+        Reader in = new FileReader("Main.java");
+
+        try {
+            IOUtils.write(IOUtils.toString(in), out);
+        } finally {
+            IOUtils.closeQuietly(in);
+            IOUtils.closeQuietly(out);
+        }
     }
 }
