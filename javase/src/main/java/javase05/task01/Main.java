a6080975ccfe4fcddc0a9206ebc6045ea6b44013
Mao Shaco <maoshaco@gmail.com>
2015-11-08 00:33:41 +0300

    Some mismatch deleted

--- javase/src/main/java/javase05/task01/Main.java
+++ javase/src/main/java/javase05/task01/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ConsoleInterface a = new ConsoleInterface();
-        a.run();
+        ConsoleInterface consoleInterface = new ConsoleInterface();
+        consoleInterface.run();
     }
 }
--- javase/src/main/java/javase05/task01/console/ConsoleInterface.java
+++ javase/src/main/java/javase05/task01/console/ConsoleInterface.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
  */
 public class ConsoleInterface {
     private final int commandSize = 3;
+
     public void run() {
 
         final FileManager fileManager = new FileManager();
@@ -16,53 +17,59 @@ public class ConsoleInterface {
 
         while (true) {
             System.out.print(fileManager.getCurrentPath() + ">");
-            Scanner in = new Scanner(System.in);
-            String buff = in.nextLine().toLowerCase();
+            try {
+                Scanner in = new Scanner(System.in);
+                String buff = in.nextLine();
 
-            if (buff.length() > commandSize - 1)
-                switch (buff.substring(0, commandSize)) {
-                    case "hlp": {
-                        System.out.println(fileManager.showCommands());
-                        break;
+                if (buff.length() > commandSize - 1)
+                    switch (buff.substring(0, commandSize)) {
+                        case "hlp": {
+                            System.out.println(fileManager.showCommands());
+                            break;
+                        }
+                        case "ccd": {
+                            fileManager.changeCurrentDirectory(buff.replaceFirst("ccd ", ""));
+                            break;
+                        }
+                        case "lsf": {
+                            System.out.println(fileManager.listFiles());
+                            break;
+                        }
+                        case "ctf": {
+                            System.out.println(fileManager.createTextFile(buff.replaceFirst("ctf ", "")));
+                            break;
+                        }
+                        case "stf": {
+                            System.out.println(fileManager.showTextFile(buff.replaceFirst("stf ", "")));
+                            break;
+                        }
+                        case "cdr": {
+                            System.out.println(fileManager.createDirectory(buff.replaceFirst("cdr ", "")));
+                            break;
+                        }
+                        case "app": {
+                            System.out.println("Input text which have to be appended");
+                            System.out.println(fileManager.appendTextToFile(buff.replaceFirst("app ", ""), in.nextLine()));
+                            break;
+                        }
+                        case "del": {
+                            System.out.println(fileManager.deleteFileOrDirectory(buff.replaceFirst("del ", "")));
+                            break;
+                        }
+                        case "ext": {
+                            return;
+                        }
+                        default:
+                            System.out.println(String.format("'%s' is not recognized as command", buff));
                     }
-                    case "ccd": {
-                        fileManager.changeCurrentDirectory(buff.replaceFirst("ccd ", ""));
-                        break;
-                    }
-                    case "lsf": {
-                        System.out.println(fileManager.listFiles());
-                        break;
-                    }
-                    case "ctf": {
-                        System.out.println(fileManager.createTextFile(buff.replaceFirst("ctf ", "")));
-                        break;
-                    }
-                    case "stf": {
-                        System.out.println(fileManager.showTextFile(buff.replaceFirst("stf ", "")));
-                        break;
-                    }
-                    case "cdr": {
-                        System.out.println(fileManager.createDirectory(buff.replaceFirst("cdr ", "")));
-                        break;
-                    }
-                    case "app": {
-                        System.out.println("Input text which have to be appended");
-                        System.out.println(fileManager.appendTextToFile(buff.replaceFirst("app ", ""), in.nextLine()));
-                        break;
-                    }
-                    case "del": {
-                        System.out.println(fileManager.deleteFileOrDirectory(buff.replaceFirst("del ", "")));
-                        break;
-                    }
-                    case "ext": {
-                        return;
-                    }
-                    default:
-                        System.out.println(String.format("'%s' is not recognized as command", buff));
-                }
-            else
-                System.out.println(String.format("'%s' is not recognized as command", buff));
+                else
+                    System.out.println(String.format("'%s' is not recognized as command", buff));
 
+            } catch (RuntimeException e) {
+                System.out.println("Something goes wrong in operation with files\nReason: " + e.getLocalizedMessage());
+            } catch (Exception e) {
+                System.out.println("Something goes wrong critical exception\nReason: " + e.getLocalizedMessage());
+            }
         }
     }
 }
